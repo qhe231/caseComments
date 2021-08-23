4c30ca75c2b9cdffabb0b11b971392dd5ace74f0
Peter He <qhe231@aucklanduni.ac.nz>
2021-08-24 11:33:04 +1200

    fix empty data query problem

--- force-app/main/default/lwc/caseCommentsCard/caseCommentsCard.js
+++ force-app/main/default/lwc/caseCommentsCard/caseCommentsCard.js
@@ -1,16 +1,24 @@
 import { LightningElement, api, wire } from 'lwc'
-import getRelatedComments from '@salesforce/apex/caseCommentController.getRelatedComments'
-import saveComment from '@salesforce/apex/caseCommentController.saveComment'
+import getRelatedComments from '@salesforce/apex/CaseCommentController.getRelatedComments'
+import saveComment from '@salesforce/apex/CaseCommentController.saveComment'
+
+import USER_FIELD from '@salesforce/schema/CaseComment.CreatedById'
+import CREATED_DATE_FIELD from '@salesforce/schema/CaseComment.CreatedDate'
+import BODY_FIELD from '@salesforce/schema/CaseComment.commentBody'
+
+const COLUMNS = [
+    { label: 'User', fieldName: USER_FIELD.fieldApiName, type: 'text' },
+    { label: 'Created Date', fieldName: CREATED_DATE_FIELD.fieldApiName, type: 'datetime' },
+    { label: 'Comment', fieldName: BODY_FIELD.fieldApiName, type: 'text' }
+]
 
 export default class CaseCommentsCard extends LightningElement {
     isModalDisplayed = false
     numCase = 0
-    @api recordId
-    @wire(getRelatedComments, {
-        caseId: '$recordId'
-    })
-    caseComments
+    columns = COLUMNS
 
+    @api recordId
+    @wire(getRelatedComments, { caseId: '$recordId' }) caseComments
 
     handleCloseModal() {
         this.isModalDisplayed = false
