febc35a2219b48b046fe93c49f0cb296b8191323
Peter He <qhe231@aucklanduni.ac.nz>
2021-08-25 08:21:04 +1200

    adjust table format from lightning datatable to table more like ootb

--- force-app/main/default/lwc/caseCommentsCard/caseCommentsCard.js
+++ force-app/main/default/lwc/caseCommentsCard/caseCommentsCard.js
@@ -1,25 +1,26 @@
-import { LightningElement, api, wire } from 'lwc'
+import { LightningElement, api, wire, track } from 'lwc'
 import { NavigationMixin } from 'lightning/navigation'
 import getRelatedComments from '@salesforce/apex/CaseCommentController.getRelatedComments'
 import saveComment from '@salesforce/apex/CaseCommentController.saveComment'
 
-import USER_FIELD from '@salesforce/schema/CaseComment.CreatedById'
+/* import USER_FIELD from '@salesforce/schema/CaseComment.CreatedById'
 import CREATED_DATE_FIELD from '@salesforce/schema/CaseComment.CreatedDate'
-import BODY_FIELD from '@salesforce/schema/CaseComment.commentBody'
+import BODY_FIELD from '@salesforce/schema/CaseComment.CommentBody'
 
 const COLUMNS = [
     { label: 'User', fieldName: USER_FIELD.fieldApiName, type: 'text' },
     { label: 'Created Date', fieldName: CREATED_DATE_FIELD.fieldApiName, type: 'datetime' },
     { label: 'Comment', fieldName: BODY_FIELD.fieldApiName, type: 'text' }
-]
+] */
 
 export default class CaseCommentsCard extends NavigationMixin(LightningElement) {
     isModalDisplayed = false
-    columns = COLUMNS
+    // columns = COLUMNS
     numCases = 0
 
     @api recordId
-    @wire(getRelatedComments, { caseId: '$recordId' }) caseComments
+    @wire(getRelatedComments, { caseId: '$recordId' })
+    caseComments
 
     handleCloseModal() {
         this.isModalDisplayed = false
